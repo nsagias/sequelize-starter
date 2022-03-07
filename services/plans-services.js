f8fa6790a8d7228b7a29a9423a2e6bcf90b947c0
Nick <nsagias@gmail.com>
2022-03-07 00:12:19 -0500

    create plan-service and scaffold outline

---
+++ services/plans-services.js
@@ -0,0 +1,11 @@
+const Plan = require("../models/index")[Plan];
+
+modules.export = class PlansService {
+  async findAll(userId) {};
+
+  async findOne(id) {};
+
+  async create(plan) {};
+
+  async deleteOne(id) {};
+}
