4ec64e9ec9d2d22517413566e347249528224454
Nick <nsagias@gmail.com>
2022-03-06 20:49:33 -0500

    create schema using generator

---
+++ migrations/20220307014855-create-subscription.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('subscriptions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      planId: {
+        type: Sequelize.INTEGER
+      },
+      coupon: {
+        type: Sequelize.STRING
+      },
+      cardNumber: {
+        type: Sequelize.STRING
+      },
+      holderName: {
+        type: Sequelize.STRING
+      },
+      expirationDate: {
+        type: Sequelize.STRING
+      },
+      cvv: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('subscriptions');
+  }
+};
---
+++ models/subscription.js
@@ -0,0 +1,40 @@
+const Joi = require("joi");
+
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class subscription extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  subscription.init({
+    planId: DataTypes.INTEGER,
+    coupon: DataTypes.STRING,
+    cardNumber: DataTypes.STRING,
+    holderName: DataTypes.STRING,
+    expirationDate: DataTypes.STRING,
+    cvv: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'subscription',
+  });
+  return subscription;
+};
+
+
+module.exports.SubscriptionsValidationSchema = Joi.object().keys({
+  planId: Joi.number().positive().required(),
+  coupon: Joi.number().mix(0).max(100).optional().allow(null),
+  cardNumber: Joi.string().creditCard().required(),
+  holderName: Joi.string().alphanum.required(),
+  expirationDate: Joi.string.required(),
+  cvv: Joi.string().min(3).max(3).required()
+});
